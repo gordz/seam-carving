7e7429e3f0b1617b23b5aef64cc2961ddd9829bf
Graham Gatus <graham.gatus@optus.com.au>
2014-11-11 11:19:28 +1100

    Adding initial empty class and pom.xml.

---
+++ src/main/java/SeamCarver.java
@@ -0,0 +1,78 @@
+
+
+public class SeamCarver {
+	
+	/**
+	 * create a seam carver object based on the given picture
+	 * @param picture
+	 */
+	public SeamCarver(Picture picture)  {
+		
+	}
+	
+	/**
+	 * current picture
+	 * @return
+	 */
+	public Picture picture() {
+		
+	}
+	
+	/**
+	 * width of current picture
+	 * @return
+	 */
+	public int width()  {
+		
+	}
+	
+	/**
+	 * height of current picture
+	 * @return
+	 */
+	public int height() {
+		
+	}
+	
+	/**
+	 * energy of pixel at column x and row y
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public double energy(int x, int y) {
+		
+	}
+	
+	/**
+	 * sequence of indices for horizontal seam
+	 * @return
+	 */
+	public int[] findHorizontalSeam() {
+		
+	}
+	
+	/**
+	 * sequence of indices for vertical seam
+	 * @return
+	 */
+	public int[] findVerticalSeam() {
+		
+	}
+	
+	/**
+	 * remove horizontal seam from current picture
+	 * @param seam
+	 */
+	public void removeHorizontalSeam(int[] seam) {
+		
+	}
+	
+	/**
+	 * remove vertical seam from current picture
+	 * @param seam
+	 */
+	public void removeVerticalSeam(int[] seam) {
+	}
+}
+
