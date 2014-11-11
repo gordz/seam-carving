251427ad7cf029a5e5f5fa83f09a41d99480babd
Graham Gatus <graham.gatus@optus.com.au>
2014-11-11 13:45:29 +1100

    Added test dependencies + test cleanup.

--- src/test/java/SeamCarverTest.java
+++ src/test/java/SeamCarverTest.java
@@ -5,12 +5,12 @@ public class SeamCarverTest {
 	private Picture picture = new Picture(10,20);
 	private SeamCarver seamCarver = new SeamCarver(picture);
 	
-	@Test (expected = IndexOutOfBoundsException.class)
+	@org.junit.Test (expected = IndexOutOfBoundsException.class)
 	public void energy_ShouldThrowIndexOutOfBoundsException_WhenXLessThan_0() {
 		seamCarver.energy(-1, 1);
 	}
 	
-	@Test (expected = IndexOutOfBoundsException.class)
+	@org.junit.Test (expected = IndexOutOfBoundsException.class)
 	public void energy_ShouldThrowIndexOutOfBoundsException_WhenYLessThan_0() {
 		seamCarver.energy(1, -1);
 	}
